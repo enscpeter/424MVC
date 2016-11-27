cf09c3801f6a460c8430361ef61a947fa63967e6
enscpeter <p.tor@live.ca>
2016-11-27 03:56:02 -0800

    Add files via upload

--- videnc.cpp
+++ videnc.cpp
@@ -13,11 +13,11 @@
  *
  * Feb. 2005
  *
- * To do:
- * 1. Intra MB in P frame: criterion?
- * 2. Better entropy coding: zigzag scanning, early termination.
- * 3. CBP
+ * Modified AGAIN by Francis Tran, Peter Le
+ * School of Engineering Science
+ * Simon Fraser University
  *
+ *Nov. 2016
  *********************************************************************************/
 
 #include <iostream>
@@ -31,6 +31,9 @@ using namespace std;
 
 #include "codeclib.h"
 
+	int iOutBytes;
+    int iGRPara = 0, iAllBytes = 0;
+
 void usage() {
     cout << "A simple video codec:" << endl;;
     cout << "Usage: videnc infile outfile width height frames qstep" << endl;
@@ -42,108 +45,151 @@ void usage() {
     cout << "    qstep:      Quantization step size (can be floating-point)" << endl;
 }
 
+
+int openin(ifstream & ipfile, char **arguv, int index){ //Function to open input files
+ipfile.open(arguv[index], ios::in|ios::binary);
+    if(!ipfile)
+    {
+        cout << "Can't open file " << arguv[index] << endl;
+        return -2;
+    }
+}
+ 
+int openout(ofstream & opfile, char **arguv, int index){ //Function to open output files
+    opfile.open(arguv[index], ios::out|ios::binary);
+    if(!opfile)
+    {
+        cout << "Can't open file " << arguv[index] << endl;
+        return -3;
+    }
+}
+
+void encode_out(ofstream & opfile, ifstream & ipfile, unsigned char *ImgBuf, IEncoder *pEnc, unsigned char *pcBSB, float Qstep, int frmsz, int Width, int Height, int Frames, ofstream & ofsD){
+   
+	//write file header
+    opfile.write((const char *) &Width, sizeof(int));
+    opfile.write((const char *) &Height, sizeof(int));
+    opfile.write((const char *) &Frames, sizeof(int));
+    opfile.write((const char *) &Qstep, sizeof(float));
+
+    for (int i = 0; i < Frames; i++) {
+
+        //read one frame
+		ipfile.read((char *)ImgBuf, frmsz);
+        pEnc->SetImage(ImgBuf);
+
+        //main routine to encode the frame
+        iOutBytes = pEnc->codeImage(i == 0, pcBSB, Qstep);
+        iAllBytes += iOutBytes;
+
+        cout << iOutBytes << endl;
+ 
+        //write one frame data to output file
+        opfile.write((const char *) &iOutBytes, sizeof(int));
+        opfile.write((const char *) pcBSB, iOutBytes);
+
+		#ifdef DUMPMV
+        if (i > 0) {
+            pEnc->DumpMV(ofsD);
+        }
+	#endif
+	}
+
+    cout << "Bits/pixel: " << std::setprecision(5) << iAllBytes * 8.0f / (Width * Height * Frames) << endl;
+
+    ipfile.close(); //close input and output files
+    //opfile.close(); //don't close for big ass z.out file with both views
+
+	#ifdef DUMPMV
+    ofsD.close();
+	#endif
+}
+
 //--------------------------------------------------------
 //
 // main routine
 //
 //--------------------------------------------------------
 int main(int argc,char **argv) {
- 
+
     ifstream ifsInfile;
     ofstream ofsOutfile;
 
-    int iWidth, iHeight, iFrames, iOutBytes;
-    int iGRPara = 0, iAllBytes = 0;
-    unsigned char *pcImgBuf, *pcBitstreamBuf;
+    unsigned char *pcImgBuf, *pcImgBuf3, *pcImgBuf5, *pcBitstreamBuf, *pcBitstreamBuf3;
     float fQstep;
 
-    if(argc < 7) {
+	int iWidth, iHeight, iFrames;
+
+	iWidth = atoi(argv[5]); //1024 for YUV
+    iHeight = atoi(argv[6]); //768 for YUV
+    iFrames = atoi(argv[7]); 
+
+	IEncoder *pEncoder = new IEncoder(iWidth, iHeight);
+	IEncoder *pEncoder3 = new IEncoder(iWidth, iHeight);
+
+    if(argc < 9) {
         usage();
         return -1;
     }
-
-    //open input file
-    ifsInfile.open(argv[1], ios::in|ios::binary);
-    if(!ifsInfile)
-    {
-        cout << "Can't open file " << argv[1] << endl;
-        return -2;
-    }
-
-    //open output file
-    ofsOutfile.open(argv[2], ios::out|ios::binary);
-    if(!ofsOutfile)
-    {
-        cout << "Can't open file " << argv[2] << endl;
-        return -3;
-    }
-
-    iWidth = atoi(argv[3]);
-    iHeight = atoi(argv[4]);
-    iFrames = atoi(argv[5]);
-
-    fQstep = (float) atof(argv[6]);
+	cout << argv[1] << " " << argv[2] << " " << argv[3] << " " <<  argv[4] << " " << endl;
+     
+	fQstep = (float) atof(argv[8]); //quantization step
 
     int iFrameSize = iWidth * iHeight * 3 / 2; //for YUV
-    pcImgBuf = new unsigned char[iFrameSize];
-    if (!pcImgBuf) {
-        cout << "Fail to create image buffer." << endl;
+
+	//open input and output files for balloons1.yuv
+	openin(ifsInfile, argv, 1);
+	openout(ofsOutfile, argv, 2);
+
+	pcImgBuf = new unsigned char[iFrameSize];
+
+	if (!pcImgBuf) {
+        cout << "Failed to create image buffer." << endl;
         return -5;
     }
 
-    pcBitstreamBuf = new unsigned char[iFrameSize];
+	pcBitstreamBuf = new unsigned char[iFrameSize];
     if (!pcBitstreamBuf) {
-        cout << "Fail to create output buffer." << endl;
+        cout << "Failed to create output buffer." << endl;
         return -6;
     }
-
-    IEncoder *pEncoder = new IEncoder(iWidth, iHeight);
 
 #ifdef DUMPMV
     ofstream ofsDump;
     ofsDump.open("mv.dat", ios::out|ios::binary);
 #endif
 
-    //write file header
-    ofsOutfile.write((const char *) &iWidth, sizeof(int));
-    ofsOutfile.write((const char *) &iHeight, sizeof(int));
-    ofsOutfile.write((const char *) &iFrames, sizeof(int));
-    ofsOutfile.write((const char *) &fQstep, sizeof(float));
+encode_out(ofsOutfile, ifsInfile, pcImgBuf, pEncoder, pcBitstreamBuf, fQstep, iFrameSize, iWidth, iHeight, iFrames, ofsDump);
 
-    for (int i = 0; i < iFrames; i++) {
-        //read one frame
-        ifsInfile.read((char *)pcImgBuf, iFrameSize);
-        pEncoder->SetImage(pcImgBuf);
+	ifstream ifsInfile3;
+	ofstream ofsOutfile3; //omit for big ass z.out
 
-        //main routine to encode the frame
-        iOutBytes = pEncoder->codeImage(i == 0, pcBitstreamBuf, fQstep);
-        iAllBytes += iOutBytes;
+	//open input file for balloons3.yuv
+	openin(ifsInfile3, argv, 3);
+    //open output file for balloons3.yuv
+	openout(ofsOutfile3, argv, 4); //Don't need to open if making big ass z.out
 
-        cout << iOutBytes << endl;
- 
-        //write one frame data to output file
-        ofsOutfile.write((const char *) &iOutBytes, sizeof(int));
-        ofsOutfile.write((const char *) pcBitstreamBuf, iOutBytes);
+	pcImgBuf3 = new unsigned char[iFrameSize];
 
-#ifdef DUMPMV
-        if (i > 0) {
-            pEncoder->DumpMV(ofsDump);
-        }
-#endif
+	if (!pcImgBuf3) {
+		cout << "Failed to create image buffer." << endl;
+		return -5;
+	}
 
+	pcBitstreamBuf3 = new unsigned char[iFrameSize];
+    if (!pcBitstreamBuf3) {
+        cout << "Failed to create output buffer." << endl;
+        return -6;
     }
 
-    cout << "Bits/pixel: " << std::setprecision(5) << iAllBytes * 8.0f / (iWidth * iHeight * iFrames) << endl;
+encode_out(ofsOutfile3, ifsInfile3, pcImgBuf3, pEncoder3, pcBitstreamBuf, fQstep, iFrameSize, iWidth, iHeight, iFrames, ofsDump);
 
-    ifsInfile.close();
-    ofsOutfile.close();
-
-#ifdef DUMPMV
-    ofsDump.close();
-#endif
+	//ofsOutfile.close(); //in the case that only one z.out is used
 
     delete pEncoder;
+	delete pEncoder3;
     delete pcImgBuf;
+	delete pcImgBuf;
     delete pcBitstreamBuf;
 
     return 0;
